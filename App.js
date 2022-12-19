47693e223e2e38013972cc613dfcda81844fbc9a
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-12-19 18:46:20 -0300

    Parte das importações

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {} from 'react-native'
 
 export default function App() {
   return (
