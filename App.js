949f183edeee8425e0005bf3cc663091b17ae932
Henrique Galdino <102200241+Henrique031@users.noreply.github.com>
2022-12-19 21:47:59 -0300

    Segundo componente de texto, adicionado

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import {} from 'react-native'
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+export default class App extends React.Component {
+  render() {
+   return (
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Segundo texto</Text>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
